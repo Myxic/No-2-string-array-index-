fcd8f0752cfe6c6abe0b734bdd80d71c5a64c182
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-11-09 16:07:48 +0100

    uplo

---
+++ No-2-string-array-index-/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+var list1 = new List<int>() { };
+
+Console.Write("Enter Words  ");
+var strings = Console.ReadLine();
+
+for (int i = 0; i < strings.Length; i++)
+{
+    char letter = strings[i];
+
+    //char letter1 = letter.TO
+    if (char.IsUpper(letter))
+    {
+        int indexxx = strings.IndexOf(letter);
+        
+        list1.Add(indexxx);
+        Console.WriteLine($"{letter } in the index {indexxx}");
+    }
+    //Console.WriteLine(list1);
+};
+
